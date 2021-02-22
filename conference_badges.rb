cd954fe1af2151cc26510cf74b012da511b970f5
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:26:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,29 +1,28 @@
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-# end
-#
-#
-# def batch_badge_creator(array)
-#   name_array = []
-#   array.each do |name|
-#     name_array << "Hello, my name is #{name}."
-#   end
-#   return name_array
-# end
-#
-#
-# def assign_rooms(array)
-#   room = []
-#   array.each_with_index do |name, index|
-#     room << "Hello, #{name}! You'll be assigned to room #{index +1}!"
-#   end
-#   return room
-# end
-
-def printer(array)
-  batch_badge_creator.each(array) do |name|
-    puts name
-  end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
 
-printer("mike")
+
+def batch_badge_creator(array)
+  name_array = []
+  array.each do |name|
+    name_array << "Hello, my name is #{name}."
+  end
+  return name_array
+end
+
+
+def assign_rooms(array)
+  room = []
+  array.each_with_index do |name, index|
+    room << "Hello, #{name}! You'll be assigned to room #{index +1}!"
+  end
+  return room
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |name|
+    puts name
+  end
+  assign_rooms()
+end
