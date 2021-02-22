a2e8131fa8ac585936024007898e9f5fb0c10917
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:06:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,12 @@ def batch_badge_creator(array)
   end
   return name_array
 end
+
+
+def assign_rooms(array)
+  room = []
+  array.each_with_index do |name, index|
+    room << "Hello, #{name}! You'll be assigned to room #{index +1}!"
+  end
+  return room
+end
