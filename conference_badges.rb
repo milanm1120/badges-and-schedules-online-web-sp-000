a6b30c8fd1b358e2827a639bd679dc4abdf8172d
Milan Mody <milanm1120@gmail.com>
2021-02-22 21:20:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,15 @@
-# def badge_maker(name)
-#   return "Hello, my name is #{name}."
-# end
-#
-#
-# def batch_badge_creator(array)
-#   name_array = []
-#   array.each do |name|
-#     name_array << "Hello, my name is #{name}."
-#   end
-#   return name_array
-# end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  name_array = []
+  array.each do |name|
+    name_array << "Hello, my name is #{name}."
+  end
+  return name_array
+end
 
 
 def assign_rooms(array)
